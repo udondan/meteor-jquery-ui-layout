d238d4cf07ec94797e76945f55f94b7eeb7502bc
udondan <deemes79@gmail.com>
2014-10-12 10:48:53 +0200

    Migrating for Meteor 0.9.x

--- package.js
+++ package.js
@@ -1,10 +1,13 @@
 Package.describe({
-  summary: "jQuery-UI Layout - The Ultimate Page Layout Manager"
+  summary: "jQuery-UI Layout - The Ultimate Page Layout Manager",
+  version: "0.1.0",
+  git: "https://github.com/udondan/meteor-jquery-ui-layout.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use('jquery', 'client');
-  api.use('jquery-ui', 'client');
+  api.use("mrt:jquery-ui@1.9.2", 'client');
   api.add_files([
     'lib/jquery.layout-1.3.0RC30.79.js'
   ], 'client');
