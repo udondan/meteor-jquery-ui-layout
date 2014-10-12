d2599572a5a6fcbfd2a390c213f73ca801484481
udondan <deemes79@gmail.com>
2014-10-12 11:04:39 +0200

    Upgraded to jQuery UI layout v1.4.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
-  summary: "jQuery-UI Layout - The Ultimate Page Layout Manager",
-  version: "0.1.0",
+  summary: "jQuery UI Layout - The Ultimate Page Layout Manager",
+  version: "1.4.3_1",
   git: "https://github.com/udondan/meteor-jquery-ui-layout.git"
 });
 
@@ -9,6 +9,7 @@ Package.on_use(function (api) {
   api.use('jquery', 'client');
   api.use("mrt:jquery-ui@1.9.2", 'client');
   api.add_files([
-    'lib/jquery.layout-1.3.0RC30.79.js'
+    'vendor/jquery-ui-layout/source/stable/jquery.layout.js',
+    'vendor/jquery-ui-layout/source/stable/layout-default.css'
   ], 'client');
 });
